341a2a746d4ce899121c05caf1ed3cee18859269
minSsan <msp1125@hanyang.ac.kr>
2024-02-06 14:41:43 +0900

    feat: init dropdown component

--- src/components/index.js
+++ src/components/index.js
@@ -4,3 +4,4 @@ export * from "./vertical-divider/vertical-divider";
 export * from "./footer/footer";
 export * from "./main-service-card/main-service-card";
 export * from "./list-board/list-board";
+export * from "./drop-down/drop-down";
