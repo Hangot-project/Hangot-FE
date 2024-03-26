ab07440a6e8d61a24a0381100db42ff03265408f
minSsan <msp1125@hanyang.ac.kr>
2024-03-26 22:04:33 +0900

    feat: redux setup

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,18 +1,30 @@
+"use client";
+
 import { Header, Footer } from "../components";
 import styles from "../styles/layout.module.css";
 import "../styles/globals.css";
+import { useRef } from "react";
+import { Provider } from "react-redux";
+import { makeStore } from "@/lib/store";
 
-export const metadata = {
+const metadata = {
   title: "한양대학교 데이터 포털",
 };
 
 export default function Layout({ children }) {
+  const storeRef = useRef();
+  if (!storeRef.current) {
+    storeRef.current = makeStore();
+  }
+
   return (
     <html lang="ko">
       <body>
         <div className={styles.container}>
           <Header />
-          <div className={styles.content}>{children}</div>
+          <Provider store={storeRef.current}>
+            <div className={styles.content}>{children}</div>
+          </Provider>
           <Footer />
         </div>
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,26 @@
-import { currentOpenDataDummy } from "../dummy-data/main-datas";
+"use client";
+
+import { useEffect, useState } from "react";
 import Home from "./home";
 
-async function getListData() {
-  // TODO: server api fetch
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-  const all = await res.json();
-  const listData = all.slice(0, 5).map((value) => ({
-    id: value.id,
-    label: "일반행정",
-    title: value.title,
-  }));
-  return listData;
-}
-
 export default async function Page() {
-  const listData = await getListData();
+  const [listData, setListData] = useState();
+
+  useEffect(() => {
+    async function fetchListData() {
+      // TODO: server api fetch
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+      const all = await res.json();
+      const listData = all.slice(0, 5).map((value) => ({
+        id: value.id,
+        label: "일반행정",
+        title: value.title,
+      }));
+      setListData(listData);
+    }
+
+    fetchListData();
+  }, []);
+
   return <Home listData={listData} />;
 }
