36270bf23e82bc83594ac828695e3b56b58866ae
minSsan <msp1125@hanyang.ac.kr>
2024-03-27 00:40:22 +0900

    fix: provider -> Client Component로 분리, app/page.jsx RSC로 원상복귀

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,30 +1,21 @@
-"use client";
-
 import { Header, Footer } from "../components";
 import styles from "../styles/layout.module.css";
 import "../styles/globals.css";
-import { useRef } from "react";
-import { Provider } from "react-redux";
-import { makeStore } from "@/lib/store";
+import ReduxProvider from "@/lib/provider";
 
-const metadata = {
+export const metadata = {
   title: "한양대학교 데이터 포털",
 };
 
 export default function Layout({ children }) {
-  const storeRef = useRef();
-  if (!storeRef.current) {
-    storeRef.current = makeStore();
-  }
-
   return (
     <html lang="ko">
       <body>
         <div className={styles.container}>
           <Header />
-          <Provider store={storeRef.current}>
-            <div className={styles.content}>{children}</div>
-          </Provider>
+          <div className={styles.content}>
+            <ReduxProvider>{children}</ReduxProvider>
+          </div>
           <Footer />
         </div>
       </body>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,26 +1,19 @@
-"use client";
-
-import { useEffect, useState } from "react";
+import { currentOpenDataDummy } from "../dummy-data/main-datas";
 import Home from "./home";
 
+async function getListData() {
+  // TODO: server api fetch
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+  const all = await res.json();
+  const listData = all.slice(0, 5).map((value) => ({
+    id: value.id,
+    label: "일반행정",
+    title: value.title,
+  }));
+  return listData;
+}
+
 export default async function Page() {
-  const [listData, setListData] = useState();
-
-  useEffect(() => {
-    async function fetchListData() {
-      // TODO: server api fetch
-      const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-      const all = await res.json();
-      const listData = all.slice(0, 5).map((value) => ({
-        id: value.id,
-        label: "일반행정",
-        title: value.title,
-      }));
-      setListData(listData);
-    }
-
-    fetchListData();
-  }, []);
-
+  const listData = await getListData();
   return <Home listData={listData} />;
 }
