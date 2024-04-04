507e18eb71f4fa6dad5f16bb35f9d423b60a4426
minSsan <msp1125@hanyang.ac.kr>
2024-04-04 15:38:50 +0900

    HYD-16 fix: 주제, 유형, 조직 배열로 전달

---
+++ src/app/search-result/page.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import SearchResult from "./search-result";
+import { getSearchResults } from "../../api/search-result";
+import { notFound } from "next/navigation";
+import { ParamToSearchResultProps } from "../../utils/param-to-search-result-props";
+
+export default async function Page({ searchParams }) {
+  const keyword = searchParams["keyword"];
+  const pageStr = searchParams["page"];
+  const theme = ParamToSearchResultProps(searchParams["theme"]);
+  const type = ParamToSearchResultProps(searchParams["type"]);
+  const organization = ParamToSearchResultProps(searchParams["organization"]);
+  const sort = searchParams["sort"];
+
+  if (pageStr && !/^[^0]\d*/.test(pageStr)) {
+    notFound();
+  }
+
+  const response = await getSearchResults(
+    keyword,
+    pageStr,
+    theme,
+    type,
+    organization,
+    sort,
+  );
+
+  // TODO: 서버 에러에 대한 페이지 별도 추가 필요
+  if (!response.success) {
+    notFound();
+  }
+
+  const result = response.result;
+  return (
+    <SearchResult
+      results={result.data}
+      totalElement={result.totalElement}
+      totalPage={result.totalPage}
+      initPage={pageStr}
+    />
+  );
+}
