b6e8dd8cb4709250320a71a841bf0dd7950c7f88
minSsan <msp1125@hanyang.ac.kr>
2024-04-06 23:46:08 +0900

    HYD-13 feat: 차트 data -> Suspense 추가 및 SSG 파라미터 타입 수정

--- src/app/search-result/[id]/page.tsx
+++ src/app/search-result/[id]/page.tsx
@@ -1,31 +1,28 @@
 import { BarChart } from "../../../components";
 import { dataset as _dataset } from "../../../dummy-data/datasets";
-import Loading from "../../../app/loading";
+import Loading from "../../loading";
 import { getAllDatasets, getDatasetDetail } from "../../../api/dataset";
+import { Suspense } from "react";
 
 export async function generateStaticParams() {
   const datasets = await getAllDatasets();
 
   return datasets.map((dataset) => ({
-    id: dataset.datasetId,
+    id: dataset.datasetId.toString(),
   }));
 }
 
-async function Page({ params }: { params: { id: number } }) {
-  const datasetDetail = await getDatasetDetail(params.id);
+export default async function Page({ params }: { params: { id: string } }) {
+  const datasetDetail = await getDatasetDetail(parseInt(params.id));
   // TODO: 데이터셋 시각화 api 연결
   const dataset = { ..._dataset };
 
   return (
     <div>
       <p>id: {params.id}</p>
-      {dataset ? (
-        <BarChart x_axis_name={dataset.x_axis_name} dataset={dataset} />
-      ) : (
-        <Loading />
-      )}
+      <Suspense fallback={<Loading />}>
+        {dataset && <BarChart x_axis_name={dataset.x_axis_name} dataset={dataset} />}
+      </Suspense>
     </div>
   );
 }
-
-export default Page;
