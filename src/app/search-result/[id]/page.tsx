0143e609727ab4f7e36389b07c0880e548211e01
minSsan <msp1125@hanyang.ac.kr>
2024-04-07 01:45:41 +0900

    HYD-13 feat: 데이터 상세정보 페이지 UI 골격 완성

--- src/app/search-result/[id]/page.tsx
+++ src/app/search-result/[id]/page.tsx
@@ -3,6 +3,7 @@ import { dataset as _dataset } from "../../../dummy-data/datasets";
 import Loading from "../../loading";
 import { getAllDatasets, getDatasetDetail } from "../../../api/dataset";
 import { Suspense } from "react";
+import styles from "./detail.module.css";
 
 export async function generateStaticParams() {
   const datasets = await getAllDatasets();
@@ -19,7 +20,96 @@ export default async function Page({ params }: { params: { id: string } }) {
 
   return (
     <div>
-      <p>id: {params.id}</p>
+      {/* //? 기본 정보 표시 */}
+      <div className={styles.mainInfoContainer}>
+        <div className={styles.titleContainer}>
+          <h1 className={styles.title}>{datasetDetail.title}</h1>
+          <div className={styles.typeBox}>JSON</div>
+        </div>
+        <p className={styles.description}>{datasetDetail.description}</p>
+        <button className={styles.downloadBtn}>데이터 다운로드</button>
+      </div>
+
+      {/* //? 데이터 정보 표시 */}
+      <h2
+        style={{
+          marginTop: "3.75rem",
+        }}
+        className={styles.subtitle}
+      >
+        데이터 정보
+      </h2>
+
+      <table
+        style={{
+          marginTop: "1.125rem",
+        }}
+        className={styles.table}
+      >
+        <colgroup>
+          <col width={"10%"} />
+          <col width={"40%"} />
+          <col width={"10%"} />
+          <col width={"40%"} />
+        </colgroup>
+        {/* 1번 행 */}
+        <tr>
+          <th>공개일자</th>
+          <td>{datasetDetail.createdDate}</td>
+          <th>최신수정일자</th>
+          <td>
+            {datasetDetail.updateDate
+              ? datasetDetail.updateDate
+              : datasetDetail.createdDate}
+          </td>
+        </tr>
+        {/* 2번 행 */}
+        <tr>
+          <th>주제</th>
+          <td>{datasetDetail.theme}</td>
+          <th>제공기관</th>
+          <td>{datasetDetail.organization}</td>
+        </tr>
+        {/* 3번 행 */}
+        <tr>
+          <th>원본형태</th>
+          <td>DB</td>
+          <th>제3제작권자</th>
+          <td>없음</td>
+        </tr>
+        {/* 4번 행 */}
+        <tr>
+          <th>라이선스</th>
+          <td colSpan={3}>
+            저작권자표시(BY) : 이용이나 변경 및 2차적 저작물의 작성을 포함한
+            자유이용을 허락합니다.
+          </td>
+        </tr>
+      </table>
+
+      {/* //? 시트 영역 */}
+      <h2
+        style={{
+          marginTop: "5rem",
+        }}
+        className={styles.sheetTitle}
+      >
+        SHEET
+      </h2>
+
+      <div className={styles.divisionLine} />
+
+      <h3
+        style={{
+          marginTop: "3rem",
+          marginBottom: "2.5rem",
+        }}
+        className={styles.sheetSubtitle}
+      >
+        {datasetDetail.title}
+      </h3>
+
+      {/* //? 데이터 시각화 */}
       <Suspense fallback={<Loading />}>
         {dataset && <BarChart x_axis_name={dataset.x_axis_name} dataset={dataset} />}
       </Suspense>
