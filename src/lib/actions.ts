aa782c009649544461527fa6f60130757580e49d
minSsan <msp1125@hanyang.ac.kr>
2024-04-28 14:21:21 +0900

    feat: getToken 함수 추가

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,7 +1,9 @@
 "use client";
 
-import { encrypt } from "../utils/secure/token";
+import { decrypt, encrypt } from "../utils/secure/token";
 // import { cookies } from "next/headers";
+
+const tokenKey = process.env.NEXT_PUBLIC_TOKEN_KEY;
 
 /**
  * 응답받은 토큰을 암호화하여 쿠키에 저장하는 메서드
@@ -12,10 +14,18 @@ import { encrypt } from "../utils/secure/token";
 export async function handleToken(grantType: string, token: string) {
   const encryptedToken = encrypt(grantType + " " + token);
 
-  localStorage.setItem("session", encryptedToken);
+  localStorage.setItem(tokenKey, encryptedToken);
 
   // cookies().set("session", encryptedToken, {
   //   // sameSite: "strict", // 배포 후 적용
   //   path: "/",
   // });
+}
+
+export async function getToken(): Promise<string | null> {
+  const item = localStorage.getItem(tokenKey);
+  if (item === null) return null;
+
+  const decryptedToken = decrypt(item);
+  return decryptedToken;
 }
