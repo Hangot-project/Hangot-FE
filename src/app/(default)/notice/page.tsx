6dd19b4f637ea7213d44c0b09ac47026c532ef06
minSsan <msp1125@hanyang.ac.kr>
2024-05-19 02:07:07 +0900

    feat: notice page 누락

---
+++ src/app/(default)/notice/page.tsx
@@ -0,0 +1,16 @@
+import { getAllNotice } from "../../../api/notice/getAllNotice";
+import { Notice } from "./notice";
+
+async function Page({ searchParams }) {
+  const sort = searchParams["sort"];
+  const page = searchParams["page"] ? parseInt(searchParams["page"]) : 0;
+
+  const response = await getAllNotice({
+    sort,
+    page,
+  });
+
+  return <Notice result={response} initPage={page} />;
+}
+
+export default Page;
