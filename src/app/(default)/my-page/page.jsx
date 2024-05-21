fdd06561dce3e2200b0600854a7c5463f29f653b
minSsan <msp1125@hanyang.ac.kr>
2024-05-21 17:59:43 +0900

    feat: 마이페이지 - 비밀번호 변경, 스크랩 내역

--- src/app/(default)/my-page/page.jsx
+++ src/app/(default)/my-page/page.jsx
@@ -4,9 +4,10 @@ import { useState, useEffect } from "react";
 import styles from "./my-page.module.css";
 import { Admin } from "../../../page-src/my-page/admin/admin";
 import { DlownloadList } from "../../../page-src/my-page/download-list/dlownload-list";
-import { ScrapList } from "../../../page-src/my-page/scrap-list/scrap-list";
+import { RequestList } from "../../../page-src/my-page/request-list/request-list";
 import "../../../page-src/my-page/my-page.css";
 import { QnA } from "../../../page-src/my-page/qna/qna";
+import { ScrapList } from "../../../page-src/my-page/scrap-list/scrap-list";
 
 export default function MyPage() {
   const [selectedMenu, setSelectedMenu] = useState("계정 관리");
@@ -20,64 +21,64 @@ export default function MyPage() {
   };
 
   return (
-    <>
-      <div className={`noLayoutPadding ${styles.container}`}>
-        {/* 마이페이지 메뉴 목록 (내 정보 관리, 데이터 관리, 문의) */}
-        <div className={styles.listWrapper}>
-          <div className={styles.list}>
-            <ol>
-              {/* 마이페이지 메뉴 1 : 내 정보 관리 */}
-              <li className={styles.detailList}>
-                <h3>내 정보 관리</h3>
-                <ol>
-                  <li onClick={() => handleMenuClick("계정 관리")}>계정 관리</li>
-                </ol>
-              </li>
+    <div className={`noLayoutPadding ${styles.container}`}>
+      {/* 마이페이지 메뉴 목록 (내 정보 관리, 데이터 관리, 문의) */}
+      <div className={styles.listWrapper}>
+        <div className={styles.list}>
+          <ol>
+            {/* 마이페이지 메뉴 1 : 내 정보 관리 */}
+            <li className={styles.detailList}>
+              <h3>내 정보 관리</h3>
+              <ol>
+                <li onClick={() => handleMenuClick("계정 관리")}>계정 관리</li>
+              </ol>
+            </li>
 
-              {/* 마이페이지 메뉴 2 : 데이터 관리 */}
-              <li className={styles.detailList}>
-                <h3>데이터 관리</h3>
-                <ol>
-                  <li onClick={() => handleMenuClick("데이터 다운로드 목록")}>
-                    데이터 다운로드 목록
-                  </li>
-                  <li onClick={() => handleMenuClick("데이터 관심 목록")}>
-                    데이터 관심 목록
-                  </li>
-                  <li onClick={() => handleMenuClick("데이터 요청 목록")}>
-                    데이터 요청 목록
-                  </li>
-                </ol>
-              </li>
+            {/* 마이페이지 메뉴 2 : 데이터 관리 */}
+            <li className={styles.detailList}>
+              <h3>데이터 관리</h3>
+              <ol>
+                <li onClick={() => handleMenuClick("데이터 다운로드 목록")}>
+                  데이터 다운로드 목록
+                </li>
+                <li onClick={() => handleMenuClick("데이터 관심 목록")}>
+                  데이터 관심 목록
+                </li>
+                <li onClick={() => handleMenuClick("데이터 요청 목록")}>
+                  데이터 요청 목록
+                </li>
+              </ol>
+            </li>
 
-              {/* 마이페이지 메뉴 3 : 문의 */}
-              <li className={styles.detailList}>
-                <h3>문의</h3>
-                <ol>
-                  <li onClick={() => handleMenuClick("Q&A")}>Q&A</li>
-                </ol>
-              </li>
-            </ol>
-          </div>
-        </div>
-
-        <div className={styles.mainWrapper}>
-          <div className={styles.mainHeader}>{selectedMenu}</div>
-          {/* 각 메뉴에 따른 섹션 조건부 렌더링 */}
-
-          {/* 계정 관리에 해당하는 섹션 */}
-          {selectedMenu === "계정 관리" && <Admin />}
-
-          {/* 데이터 다운로드 목록에 해당하는 섹션 */}
-          {selectedMenu === "데이터 다운로드 목록" && <DlownloadList />}
-
-          {/* 데이터 관심 목록에 해당하는 섹션 */}
-          {selectedMenu === "데이터 관심 목록" && <ScrapList />}
-
-          {/* Q&A에 해당하는 섹션 */}
-          {selectedMenu === "Q&A" && <QnA />}
+            {/* 마이페이지 메뉴 3 : 문의 */}
+            <li className={styles.detailList}>
+              <h3>문의</h3>
+              <ol>
+                <li onClick={() => handleMenuClick("Q&A")}>Q&A</li>
+              </ol>
+            </li>
+          </ol>
         </div>
       </div>
-    </>
+
+      <div className={styles.mainWrapper}>
+        <div className={styles.mainHeader}>{selectedMenu}</div>
+        {/* 각 메뉴에 따른 섹션 조건부 렌더링 */}
+
+        {/* 계정 관리에 해당하는 섹션 */}
+        {selectedMenu === "계정 관리" && <Admin />}
+
+        {/* 데이터 다운로드 목록에 해당하는 섹션 */}
+        {selectedMenu === "데이터 다운로드 목록" && <DlownloadList />}
+
+        {/* 데이터 관심 목록에 해당하는 섹션 */}
+        {selectedMenu === "데이터 관심 목록" && <ScrapList />}
+
+        {selectedMenu === "데이터 요청 목록" && <RequestList />}
+
+        {/* Q&A에 해당하는 섹션 */}
+        {selectedMenu === "Q&A" && <QnA />}
+      </div>
+    </div>
   );
 }
--- src/page-src/my-page/scrap-list/scrap-list.tsx
+++ src/page-src/my-page/scrap-list/scrap-list.tsx
@@ -1,36 +1,77 @@
+import React, { Suspense, useEffect, useState } from "react";
 import styles from "./scrap-list.module.css";
 import { useSession } from "next-auth/react";
-import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+import { Scrap } from "../../../types/scrap";
+import { getAllScrap } from "../../../api/scrap/getAllScrap";
+import { ScrapListResponse } from "../../../api/scrap/type";
+import Link from "next/link";
 
 export function ScrapList() {
   const { data: session, status } = useSession();
+  const router = useRouter();
 
-  useEffect(() => {}, []);
+  const [scrapList, setScrapList] = useState<Scrap[]>([]);
+
+  useEffect(() => {
+    if (status !== "authenticated") {
+      alert("로그인이 필요한 서비스입니다.");
+      router.push("/login");
+    }
+
+    async function fetchData() {
+      const response = await getAllScrap(
+        session.user.grantType,
+        session.user.accessToken,
+      );
+
+      if (!response.ok) {
+        alert(`스크랩 내역을 불러오는데 실패했습니다.\n다시 시도해주세요.`);
+        return;
+      }
+
+      const result: ScrapListResponse = await response.json();
+      setScrapList(result.result);
+    }
+
+    fetchData();
+  }, []);
 
   return (
     <div className={"mainBody"}>
       {/* 데이터 검색 결과 개수 */}
       <div className={"resultNum"}>
-        <div className={"resultNumText"}>0개의 데이터</div>
+        <div className={"resultNumText"}>
+          {scrapList.length.toLocaleString()} 개의 스크랩 내역
+        </div>
       </div>
 
-      {/* 데이터 검색 결과 */}
-      {/* TODO : 데이터 검색 결과 불러오기 */}
       <div className={"searchResult"}>
         <div className={"searchResultBody"}>
           <table>
             <thead>
               <tr>
-                <th className={styles.gridCol1}>처리상태</th>
-                <th className={styles.gridCol2}>데이터명</th>
+                <th className={styles.gridCol3}>제목</th>
+                <th className={styles.gridCol4}>데이터 유형</th>
+                <th className={styles.gridCol5}>데이터 설명</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td colSpan={2}>
-                  데이터가 없습니다. 다른 검색 조건을 선택해주세요.
-                </td>
-              </tr>
+              {scrapList.length === 0 ? (
+                <td colSpan={3}>스크랩 내역이 존재하지 않습니다.</td>
+              ) : (
+                scrapList.map((scrap) => (
+                  <tr>
+                    <td>
+                      <Link href={`/search-result/${scrap.datasetId}`}>
+                        {scrap.title}
+                      </Link>
+                    </td>
+                    <td>{scrap.type ? scrap.type : "X"}</td>
+                    <td>{scrap.description}</td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
