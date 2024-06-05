ccfcb86e736168970da1fe595f9726418680afc7
minSsan <msp1125@hanyang.ac.kr>
2024-06-05 11:09:50 +0900

    feat: 마이페이지 이름

--- src/page-src/my-page/admin/admin.tsx
+++ src/page-src/my-page/admin/admin.tsx
@@ -4,12 +4,15 @@ import { Dispatch, SetStateAction, useState } from "react";
 import styles from "./admin.module.css";
 import Nickname from "./nickname/nickname";
 import Password from "./password/password";
+import { useSession } from "next-auth/react";
 
 interface Props {
   setMenu?: Dispatch<SetStateAction<string>>;
 }
 
 export function Admin({ setMenu }: Props) {
+  const { data: session } = useSession();
+
   const [selectedPopup, setSelectedPopup] = useState(null);
 
   const handleNameChangeClick = () => {
@@ -32,9 +35,7 @@ export function Admin({ setMenu }: Props) {
         <div className={styles.pwInfo}>
           <div className={styles.idInfo}>
             <div>
-              <p>이정민</p>
-              <p>님</p>
-              <p>dalnimjm@naver.com</p>
+              <p>{session.user.name}</p>
             </div>
             <div>
               <button onClick={() => setMenu("데이터 다운로드 목록")}>
