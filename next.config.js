4ae20714a42c712974ba52f7e2c961aa48c47919
mintaek22 <biba99@naver.com>
2025-07-19 20:45:13 +0900

    fix : CI 스크립트 환경변수 수정

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     return [
       {
         source: "/api/datastore/:path*",
-        destination: `${process.env.NEXT_PUBLIC_DATASET_API}/api/datastore/:path*`
+        destination: `${process.env.NEXT_PUBLIC_DATASET_API}/api/datastore/:path*`,
       },
       {
         source: "/api/:path((?!auth).*)",
